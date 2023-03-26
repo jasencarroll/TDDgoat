ffda34cfea1352f8c58839e4ca613604b4955ad3
Jasen Carroll <108644550+jasencarroll@users.noreply.github.com>
2023-03-25 19:42:12 -0700

    it helps to be running localhost when you're trying to connect to it, it helps to read

--- functional_tests.py
+++ functional_tests.py
@@ -1,12 +1,8 @@
 from selenium import webdriver
-#from selenium.webdriver.firefox.options import Options
-#options = Options()
-#options.binary_location = r"C:/Program Files/Mozilla Firefox/Firefox.exe"
-options = webdriver.ChromeOptions()
-options.page_load_strategy = 'none'
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
-browser = webdriver.Chrome(options=options)
-#browser = webdriver.Firefox(options=options)
+from selenium.webdriver.firefox.options import Options
+options = Options()
+options.binary_location = r"C:/Program Files/Mozilla Firefox/Firefox.exe"
+browser = webdriver.Firefox(options=options)
 browser.get('http://127.0.0.1:8000')
 
-#assert 'Django' in browser.title
+assert 'Django' in browser.title
